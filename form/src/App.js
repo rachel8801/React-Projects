633d78eb6223ca03a0af48f6654a3fccbade50c5
Hongping Lin <rachel8801@gmail.com>
2021-04-14 14:36:14 -0400

    add form project

---
+++ form/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ControlledForm from './controlled-inputs';
+import MultipleInputForm from './multiple-inputs';
+
+function App() {
+  return (
+    <div className='container'>
+      {/* <ControlledForm /> */}
+      <MultipleInputForm />
+    </div>
+  );
+}
+
+export default App;
