e99fa87384df708aa03a6e673f0f5dbc14ee8188
Hongping Lin <rachel8801@gmail.com>
2021-04-23 12:49:16 -0400

    Completed all displays for the single-room pages

--- resort/src/pages/SingleRoom.js
+++ resort/src/pages/SingleRoom.js
@@ -41,14 +41,50 @@ export default class SingleRoom extends Component {
       pets,
       images,
     } = room;
+    const [mainImg, ...defaultImgs] = images;
+    console.log(defaultImgs);
     return (
-      <StyledHero img={images[0]}>
-        <Banner title={`${name} room`}>
-          <Link to='/rooms' className='btn-primary'>
-            back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={mainImg}>
+          <Banner title={`${name} room`}>
+            <Link to='/rooms' className='btn-primary'>
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className='single-room'>
+          <div className='single-room-images'>
+            {defaultImgs.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+          <div className='single-room-info'>
+            <article className='desc'>
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className='info'>
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>size : {size} SQFT</h6>
+              <h6>
+                max capacity :{' '}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+              <h6>{breakfast && 'free breakfast included'}</h6>
+            </article>
+          </div>
+        </section>
+        <section className='room-extras'>
+          <h6>extras</h6>
+          <ul className='extras'>
+            {extras.map((item, index) => (
+              <li key={index}>- {item}</li>
+            ))}
+          </ul>
+        </section>
+      </>
     );
   }
 }
